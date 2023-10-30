3c765b0fb69b26f4de1305ed47fb494aa8c7a8d7
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-10-30 13:44:22 -0500

    Update Stupidity.js

--- Stupidity/Stupidity.js
+++ Stupidity/Stupidity.js
@@ -1,1 +1,54 @@
+class stupidity { 
+  getInfo() {
+    return {
+      id: 'utilityfunctionsaddon',
+      name: 'Extra Utilities',
+      blocks: [
+        {
+          opcode: 'converttextcase',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'convert [TEXT] to [CASE]',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Hello'
+            },
+            CASE: {
+              type: Scratch.ArgumentType.STRING,
+              menu: 'STRING_CASE_MENU'
+            },
+          },
+        },
+        {
+          opcode: 'somethin',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'fetch data from [TARGETURL] with method GET',
+          arguments: {
+            TARGETURL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'https://extensions.turbowarp.org/hello.txt'
+            },
+          },
+        },
+      ],
+      menus: {
+        STRING_CASE_MENU: {
+          acceptReporters: true,
+          items: ['Uppercase', 'Lowercase']
+        },
+      },
+    };
+  }
 
+  converttextcase(args) {
+    if (args.CASE === 'Uppercase') {
+      return args.TEXT.toString().toUpperCase();
+    } else {
+      return args.TEXT.toString().toLowerCase();
+    }
+  }
+  fetchdatafromurl(args) {
+
+  }
+}
+Scratch.extensions.register(new stupidity());
