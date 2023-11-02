7e7fd38d8a852c9f7bab93ddafb28425c52ba6f3
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-02 13:33:23 -0500

    Update Stupidity.js

--- Stupidity/Stupidity.js
+++ Stupidity/Stupidity.js
@@ -48,6 +48,11 @@ class stupidity {
             },
           },
         },
+        {
+          opcode: 'invertscratch',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Invert Scratch (MAY BE PERMANENT)'
+        },
       ],
       menus: {
         STRING_CASE_MENU: {
@@ -83,7 +88,26 @@ class stupidity {
     }
   }
   writetoclipboard(args) {
-    
+    return args.FUNNIESINPUT;
+  }
+  invertscratch() {
+    // the css we are going to inject
+    var css = 'html {-webkit-filter: invert(100%);' +
+    '-moz-filter: invert(100%);' + 
+    '-o-filter: invert(100%);' + 
+    '-ms-filter: invert(100%); }',
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    // a hack, so you can "invert back" clicking the bookmarklet again
+    if (!window.counter) { window.counter = 1;} else  { window.counter ++; if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}};
+    style.type = 'text/css';
+    if (style.styleSheet){
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+    //injecting the css to the head
+    head.appendChild(style);
   }
 }
 Scratch.extensions.register(new stupidity());
