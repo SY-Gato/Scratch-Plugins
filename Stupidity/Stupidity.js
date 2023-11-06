47048d098815277760df38bcdc612e9f4572d7cc
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-06 10:29:41 -0600

    Update Stupidity.js

--- Stupidity/Stupidity.js
+++ Stupidity/Stupidity.js
@@ -59,7 +59,10 @@ class stupidity {
           text: 'Inject CSS [CSSTOINJECT] (not dangerous)',
           arguments: {
             CSSTOINJECT: {
-              type: Scratch
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'filter: contrast(2);'
+            },
+          },
         },
         {
           opcode: 'alerttext',
@@ -139,16 +142,14 @@ class stupidity {
     //injecting the css to the head
     head.appendChild(style);
   }
-  injectjs() {
-    try {
-      var injectjs = "R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200; x5=300; y5=200; var DI= document.getElementsByTagName('div'); DIL=DI.length; function A(){for(i=0; i<DIL; i++){DIS=DI[ i ].style; DIS.position='absolute'; DIS.left=Math.sin(R*x1+i*x2+x3)*x4+x5+'px'; DIS.top=Math.cos(R*y1+i*y2+y3)*y4+y5+'px'}R++}tag=setInterval('A()',5 );document.onmousedown=function(){clearInterval(tag);for(i=0; i<DIL; i++){DI[i].style.position='static';}};",
-      head = document.getElementsByTagName('head')[0],
-      script = document.createElement('script');
-      script.type = 'text/javascript';
-      head.appendChild(script);
-    } catch(e) {
-      alert(e);
-    }
+  injectcustomcss() {
+    // the css we are going to inject
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    // a hack, so you can "invert back" clicking the bookmarklet again
+    style.appendChild(document.createTextNode(css));
+    //injecting the css to the head
+    head.appendChild(style);
   }
   alerttext(args) {
     alert(args.ALERTTEXT);
