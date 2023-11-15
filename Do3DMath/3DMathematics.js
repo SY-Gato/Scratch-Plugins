05237b1ee09495abc80da82ba590059df29d11cf
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-15 11:02:05 -0600

    Create 3DMathematics.js

---
+++ Do3DMath/3DMathematics.js
@@ -0,0 +1,17 @@
+(function(Scratch) {
+  "use strict";
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('3DMathematics must run unsandboxed');
+  }
+  // Required Variables
+  let Cam3DxPos;
+  let Cam3DyPos;
+  let Cam3DzPos;
+  let AllBlockRotX;
+  let AllBlockRotY;
+  // Blocks
+  class Mathematics3D
+    getInfo() {
+      return {
+      
+})(Scratch);
