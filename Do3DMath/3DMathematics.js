a2a3221de4936b5e265906c30be8af71243408ec
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-15 13:19:52 -0600

    Update 3DMathematics.js

--- Do3DMath/3DMathematics.js
+++ Do3DMath/3DMathematics.js
@@ -18,131 +18,14 @@
         blocks: [
           {
             opcode: 'allblockrotx',
-            (function(Scratch) {
-  "use strict";
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error('3DMathematics must run unsandboxed');
-  }
-  // Required Variables
-  let Cam3DxPos;
-  let Cam3DyPos;
-  let Cam3DzPos;
-  let AllBlockRotX;
-  let AllBlockRotY;
-  // Blocks
-  class Mathematics3D {
-    getInfo() {
-      return {
-        id: '3dmathematicssystem',
-        name: '3D Mathematics System',
-        blocks: [
-          {
-            opcode: 'allblockrotx',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'All Block RotX'
-          },
-          {
-            opcode: 'goto3dcord',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Draw point at [X] [Y] [Z] in 3D',
-            arguments: {
-              X: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              Y: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              Z: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-            },
-          },
-        ],
-      };
-    }
-    allblockrotx() {
-      return AllBlockRotX;
-    }
-    goto3dcord(args) {
-      console.log(args.X);
-      console.log(args.Y);
-      console.log(args.Z);
-    }
-  }
-  Scratch.extensions.register(new Mathematics3D());
-})(Scratch);
-(function(Scratch) {
-  "use strict";
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error('3DMathematics must run unsandboxed');
-  }
-  // Required Variables
-  let Cam3DxPos;
-  let Cam3DyPos;
-  let Cam3DzPos;
-  let AllBlockRotX;
-  let AllBlockRotY;
-  // Blocks
-  class Mathematics3D {
-    getInfo() {
-      return {
-        id: '3dmathematicssystem',
-        name: '3D Mathematics System',
-        blocks: [
-          {
-            opcode: 'allblockrotx',
             blockType: Scratch.BlockType.REPORTER,
             text: 'All Block RotX'
           },
         ],
       };
-    },
-    allblockrotx() {
-      return AllBlockRotX;
-    }
-    goto3dcord(args) {
-      console.log(args.X);
-      console.log(args.Y);
-      console.log(args.Z);
-    }
-  }
-  Scratch.extensions.register(new Mathematics3D());
-})(Scratch);
-: Scratch.BlockType.REPORTER,
-            text: 'All Block RotX'
-          },
-          {
-            opcode: 'goto3dcord',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Draw point at [X] [Y] [Z] in 3D',
-            arguments: {
-              X: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              Y: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              Z: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-            },
-          },
-        ],
-      };
     }
     allblockrotx() {
       return AllBlockRotX;
-    }
-    goto3dcord(args) {
-      console.log(args.X);
-      console.log(args.Y);
-      console.log(args.Z);
     }
   }
   Scratch.extensions.register(new Mathematics3D());
