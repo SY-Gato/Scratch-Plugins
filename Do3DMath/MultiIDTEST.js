a0d33a0b0583f14edeef49dcac29acac6fcb39bd
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-16 13:12:58 -0600

    Create MultiIDTEST.js

---
+++ Do3DMath/MultiIDTEST.js
@@ -0,0 +1,40 @@
+(function(Scratch) {
+  "use strict";
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('3DMathematics must run unsandboxed');
+  }
+  // Required Variables
+  let Cam3DxPos = 0;
+  let Cam3DyPos = 0;
+  let Cam3DzPos = 0;
+  let AllBlockRotX = 0;
+  let AllBlockRotY = 0;
+  // Blocks
+  class Mathematics3D {
+    getInfo() {
+      return {
+        id: '3dmathematicssys',
+        name: '3D Mathematics System',
+        blocks: [
+          {
+            opcode: 'camxposreturn',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Cam XPos'
+          },
+          {
+            opcode: 'allblockrotx',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'All Block RotX'
+          },
+        ],
+      };
+    }
+    camxposreturn() {
+      return Cam3DxPos;
+    }
+    allblockrotx() {
+      return AllBlockRotX;
+    }
+  }
+  Scratch.extensions.register(new Mathematics3D());
+})(Scratch);
